7f78cbcc7917ced46d44fdb233c1bbeaf61499bf
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-10-31 10:47:49 -0700

    added some words

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const server = require('./api/server.js');
+require('dotenv').config();
 
+const server = require('./api/server.js');
 
 //port coming from environment, it'll be set by Heroku
 const port = process.env.PORT || 4000;
+
 console.log('Current Port: ', port);
+console.log('\n Process Message: ', process.env.MSG);
+
 server.listen(port, () => {
   console.log(`\n*** I\'m ${port} and I\'ll be you\'re host today. ***\n`);
 });
